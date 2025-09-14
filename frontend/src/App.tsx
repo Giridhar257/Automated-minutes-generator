35eda5d2dbde6c05397d94f7c9dad8e9d1eff5e1
Giridhar <giridharrachepalli967@gmail.com>
2025-09-14 19:14:19 +0530

    update app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -143,7 +143,7 @@ function App() {
     try {
       setLoading(true);
       setGeneratedData(null);
-      const BACKEND_URL = import.meta.env.VITE_BACKEND_URL as string;
+      const BACKEND_URL = "https://automated-minutes-generator-4.onrender.com";
       const response = await fetch(`${BACKEND_URL}/generate-minutes/`, {
         method: "POST",
         body: formData,
